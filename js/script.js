4a3cac82d7a4ba285b892dd66537cf44ffa9833d
Kirill <kir.esin2011@yandex.ru>
2023-04-12 00:04:43 +0300

    i npm and json-server

--- js/script.js
+++ js/script.js
@@ -300,4 +300,8 @@ window.addEventListener("DOMContentLoaded", () => {
       closeModal()
     }, 4000)
   }
+
+  fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res))
 })
