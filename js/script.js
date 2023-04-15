4f1630c5606d28f65c1b088c98e353d50df50af6
Kirill <kir.esin2011@yandex.ru>
2023-04-15 21:47:55 +0300

    add slider indicators

--- js/script.js
+++ js/script.js
@@ -325,6 +325,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Slider
 
   const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -354,6 +355,40 @@ window.addEventListener('DOMContentLoaded', () => {
     slide.style.width = width
   })
 
+  slider.style.position = 'relative'
+
+  const indicators = document.createElement('ol'),
+        dots = []
+
+  indicators.classList.add('carousel-indicators')
+  slider.append(indicators)
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li')
+    dot.setAttribute('data-slide-to', i + 1)
+    dot.classList.add('dot')
+
+    if (i == 0) {
+      dot.style.opacity = 1
+    }
+
+    indicators.append(dot)
+    dots.push(dot)
+  }
+
+  function changeDotsOpacity() {
+    dots.forEach(dot => dot.style.opacity = '.5')
+    dots[slideIndex - 1].style.opacity = 1
+  }
+
+  function getCurrentSliderNumber() {
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`
+    } else {
+      current.textContent = slideIndex
+    }
+  }
+
   next.addEventListener('click', () => {
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
       offset = 0
@@ -369,11 +404,8 @@ window.addEventListener('DOMContentLoaded', () => {
       slideIndex++
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`
-    } else {
-      current.textContent = slideIndex
-    }
+    getCurrentSliderNumber()
+    changeDotsOpacity()
   })
 
   prev.addEventListener('click', () => {
@@ -391,10 +423,21 @@ window.addEventListener('DOMContentLoaded', () => {
       slideIndex--
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`
-    } else {
-      current.textContent = slideIndex
-    }
+    getCurrentSliderNumber()
+    changeDotsOpacity()
+  })
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', (event) => {
+      const slideTo = event.target.getAttribute('data-slide-to')
+
+      slideIndex = slideTo
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+
+      slidesField.style.transform = `translateX(-${offset}px)`
+
+      getCurrentSliderNumber()
+      changeDotsOpacity()
+    })
   })
 })
